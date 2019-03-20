5288e32d667edbe8e77c76013e2c2794c7ee3ce9
Jeremy Soller <jeremy@system76.com>
2019-03-20 16:32:41 -0600

    Cleanup sdcc example

--- examples/sdcc.c
+++ examples/sdcc.c
@@ -3,30 +3,19 @@ void putchar(char c) {
 }
 
 void puts(const char * s) {
-    while (1) {
-        char c = *s;
-        if (c == 0) {
-            break;
-        }
-        putchar(c);
-        s++;
-    }
-    /*
     char c;
     while (c = *(s++)) {
         putchar(c);
     }
-    */
 }
 
 void shutdown() {
     *((char *)0xFFFF) = 1;
 }
 
-const char * static_string = "Test";
+const char * static_string = "A Test\n";
 
 void main() {
-    putchar('A');
     puts(static_string);
     shutdown();
 }
