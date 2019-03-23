e6fe9638f1a88bda596ce068b56afe33382b9ced
Jeremy Soller <jeremy@system76.com>
2019-03-22 22:29:07 -0600

    Add description of memory

--- src/addr.rs
+++ src/addr.rs
@@ -1,7 +1,17 @@
 #[derive(Clone, Copy)]
 pub enum Addr {
+    /// Registers
+    /// 256 bytes, accessed with direct access
+    /// Low 128 bytes are shared with internal ram, high 128 bytes are special function registers
     Reg(u8),
+    /// Internal RAM
+    /// 256 bytes, accessed with indirect access
+    /// Low 128 bytes are also accessible with direct access
     IRam(u8),
+    /// Program memory
+    /// 65536 bytes, accessed with movc, read-only
     PMem(u16),
+    /// External RAM
+    /// 65536 bytes, accessed with movx
     XRam(u16),
 }
